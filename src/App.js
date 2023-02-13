f2e73d05a31f85245b98bcc08d7f8048a3dca9a0
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-13 12:39:40 -0500

    add list items to page using map

--- src/App.js
+++ src/App.js
@@ -11,6 +11,22 @@ function App() {
       <div className="container">
         <h1>Gift List {gift.length}</h1>
       </div>
+      {gift.map((element) => {
+        const { id, gift, image } = element;
+
+        return (
+          <div key={id}>
+            <div className="container">
+              <h2>
+                {id} - {gift}
+              </h2>
+            </div>
+            <div className="container">
+              <img src={image} alt="gifts" width="300px" />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
