f62125b66d0dc494bc83712eff02e5516946ffc2
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-02-13 12:29:46 -0500

    create h1 element with numbers of gifts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { data } from "./data/data";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [gift, setGift] = useState(data);
+  console.log(data);
+
+  return (
+    <div>
+      <div className="container">
+        <h1>Gift List {gift.length}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
